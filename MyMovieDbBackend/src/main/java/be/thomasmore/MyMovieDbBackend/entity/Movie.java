3c19f1f5a2451f1851b1d3d56546f0d7c081c506
Julian <r0697475@student.thomasmore.be>
2019-10-11 12:19:46 +0200

    test

---
+++ MyMovieDbBackend/src/main/java/be/thomasmore/MyMovieDbBackend/entity/Movie.java
@@ -0,0 +1,23 @@
+package be.thomasmore.MyMovieDbBackend.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="movie")
+@Data
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="title")
+    private String title;
+}
+

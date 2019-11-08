b8dba621b5ceea5f77a4a8d65a9a120d768aa5f2
Jasper <jasper.claes@hotmail.com>
2019-11-08 10:22:32 +0100

    dealers get's set's toegevoegd

--- dealers-service/src/main/java/be/thomasmore/dealersservice/entity/Dealer.java
+++ dealers-service/src/main/java/be/thomasmore/dealersservice/entity/Dealer.java
@@ -20,5 +20,86 @@ public class Dealer {
     private String phone;
     private String manufacturer;
 
+    public Dealer() {
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostal() {
+        return postal;
+    }
+
+    public void setPostal(String postal) {
+        this.postal = postal;
+    }
+
+    public String getProvence() {
+        return provence;
+    }
+
+    public void setProvence(String provence) {
+        this.provence = provence;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 }

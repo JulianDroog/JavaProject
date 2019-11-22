8aea27759d666a1e40689a9ee691e59d65c66cb7
Eline777 <eline.leysen@hotmail.be>
2019-11-22 13:18:45 +0100

    nieuwe favorite service

---
+++ favorite-service/src/main/java/be/thomasmore/favoriteservice/FavoriteServiceApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.favoriteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteServiceApplication.class, args);
+	}
+
+}
